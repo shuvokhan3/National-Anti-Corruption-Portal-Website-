6aff756c850ec2a203703acd4bf525da9dec1fa6
shuvokhan3 <shuvoj360@gmail.com>
2025-05-02 20:47:16 +0600

    Parent blade page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
-
 <head>
-
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <title>National-ACP</title>
@@ -13,11 +10,15 @@
     <!-- Favicons -->
     <link href="{{asset('assets/img/favicon.png')}}" rel="icon">
     <link href="{{asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+    <link rel="icon" type="image/x-icon" href="{{asset('/favicon.ico')}}" />
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
     <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- jQuery (must be loaded before Bootstrap and DataTables) -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 
     <!-- Vendor CSS Files -->
     <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
@@ -27,27 +28,27 @@
     <link href="{{asset('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
 
 
-    <!-- Main CSS File -->
+    <!-- Custom CSS -->
     <link href="{{asset('assets/css/main.css')}}" rel="stylesheet">
-    <link href={{asset('assets/css/jquery.dataTables.min.css')}}>
-    <script src="{{asset('assets/js/jquery-3.7.0.min.js')}}"></script>
-    <script src="{{asset('assets/js/jquery.dataTables.min.js')}}"></script>
+    <link href="{{asset('assets/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/animate.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/fontawesome.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/toastify.min.css')}}" rel="stylesheet">
 
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 
-    <link rel="icon" type="image/x-icon" href="{{asset('/favicon.ico')}}" />
-    <link href="{{asset('assets/css/bootstrap.css')}}" rel="stylesheet" />
-    <link href="{{asset('assets/css/animate.min.css')}}" rel="stylesheet" />
-    <link href="{{asset('assets/css/fontawesome.css')}}" rel="stylesheet" />
-    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" />
-    <link href="{{asset('assets/css/toastify.min.css')}}" rel="stylesheet" />
+    <!-- Custom JS -->
     <script src="{{asset('assets/js/toastify-js.js')}}"></script>
     <script src="{{asset('assets/js/axios.min.js')}}"></script>
     <script src="{{asset('assets/js/config.js')}}"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
+
 
 </head>
 
 <body class="index-page">
-
 @include('components.navbar')
 
 <div id="loader" class="LoadingOverlay d-none">
@@ -56,43 +57,43 @@
     </div>
 </div>
 
-
 <div id="content-div">
     @yield('content')
 </div>
 
 <!-- Scroll Top -->
-<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center">
+    <i class="bi bi-arrow-up-short"></i>
+</a>
 
-<!-- Vendor JS Files -->
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script>
-<script src="assets/vendor/aos/aos.js"></script>
-<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-
-<!-- Bootstrap JS Bundle with Popper -->
+<!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-<!-- AOS JS -->
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+
+<!-- Other Vendor JS Files -->
+<script src="{{asset('assets/vendor/php-email-form/validate.js')}}"></script>
+<script src="{{asset('assets/vendor/aos/aos.js')}}"></script>
+<script src="{{asset('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+<script src="{{asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+<script src="{{asset('assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+
+
+
 <script>
     AOS.init();
-</script>
-
-<!-- GLightbox JS -->
-<script src="https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js"></script>
-<script>
     const lightbox = GLightbox();
+
+    // Add diagnostic code to check DataTables loading
+    $(document).ready(function() {
+        console.log("Document ready");
+        console.log("jQuery version:", $.fn.jquery);
+        console.log("DataTable exists:", typeof $.fn.DataTable === 'function');
+    });
 </script>
-
-<!-- Your custom JS -->
-<script src="{{ asset('assets/js/main.js') }}"></script>
-
-
-<script src="{{asset('assets/js/bootstrap.bundle.js')}}"></script>
 
 @include('components.footer')
-
 </body>
+</html>
