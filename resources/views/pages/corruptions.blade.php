7fbe347f8f18c68676b465113be73475ae9f0764
shuvokhan3 <shuvoj360@gmail.com>
2025-05-04 21:56:04 +0600

    corruption page

--- resources/views/pages/corruptions.blade.php
+++ resources/views/pages/corruptions.blade.php
@@ -3,5 +3,6 @@
     @include('components.navbar')
     @include('components.space')
     @include('components.corruption')
+    @include('components.reportDetails')
     @include('components.space')
 @endsection
